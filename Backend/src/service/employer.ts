cf71b314287d3b855ffbdaf61985541fdebc99a8
Hoàng Văn Hải <hoanggvanhaii0705@gmail.com>
2026-03-16 21:31:55 +0700

    feat: create employer

---
+++ Backend/src/service/employer.ts
@@ -0,0 +1,9 @@
+import pool from "../config/database";
+import { Employer } from "../interface/employer";
+
+export const createEmployer = async (employer: Employer) => {
+    const query = "INSERT INTO employers (EmployerID, CompanyID, Position) VALUES (?, ?, ?)";
+    const values = [employer.EmployerID, employer.CompanyID, employer.Position];
+    const [result]: any = await pool.query(query, values);
+    return result.insertId;
+}
