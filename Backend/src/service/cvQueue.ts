243e18b2e1d7b3d3939f6d02a1cf5a96476ea2b3
Huy Vui <tranhuyvui714@gmail.com>
2026-04-01 15:30:36 +0700

    fix: Update Gemini AI jobApplication

---
+++ Backend/src/service/cvQueue.ts
@@ -0,0 +1,39 @@
+import { analyzeApplicationWithAI } from "./jobApplycation";
+
+interface QueueItem {
+    ApplicationID: number;
+    JobID: number;
+    CandidateID: number;
+}
+const queue: QueueItem[] = [];
+let isProcessing = false;
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const processQueue = async () => {
+    if (isProcessing) return;
+    isProcessing = true;
+    while (queue.length > 0) {
+        const currentJob = queue[0];
+        if (currentJob) {
+            try {
+                await analyzeApplicationWithAI(currentJob.ApplicationID, currentJob.JobID, currentJob.CandidateID);
+                queue.shift();
+                await sleep(3000);
+            } catch (error: any) {
+                if (error.status === 429) {
+                    console.log('Queue quá giới hạn API (429). Tạm dừng 30s');
+                    await sleep(30000);
+                }
+                else {
+                    console.error(`[Queue] Lỗi khi chấm ApplicationID ${currentJob.ApplicationID}:`, error);
+                    queue.shift();
+                }
+            }
+        }
+    }
+    isProcessing = false;
+}
+export const addJobToQueue = (ApplicationID: number, JobID: number, CandidateID: number) => {
+    queue.push({ ApplicationID, JobID, CandidateID });
+    processQueue();
+}
